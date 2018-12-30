002ed03bdd99015a932d66c669f09df35f647fd2
Hobson Lane <hobs@totalgood.com>
2018-12-29 22:27:34 -0800

    django-extensions and models

--- src/knowledgequest/settings.py
+++ src/knowledgequest/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'knowledgequest.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '..', '..', '..', 'src', 'springboard', 'tannistha', 'enron_email.db'),
     }
 }
 
