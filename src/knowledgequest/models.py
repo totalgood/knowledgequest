3e904a6c94cdc612d8d5a0e598afc0a6d6270e0c
Hobson Lane <hobs@totalgood.com>
2018-12-30 15:45:27 -0800

    new models

--- src/knowledgequest/models.py
+++ src/knowledgequest/models.py
@@ -6,9 +6,14 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+from tqdm import tqdm
+import sqlite3
+import keyword
+keywords = set(keyword.kwlist)
 
 
 class Address(models.Model):
+    id = models.AutoField(primary_key=True)
     address = models.TextField(blank=True, null=True)
     emailid = models.BigIntegerField(blank=True, null=True)
 
@@ -19,6 +24,7 @@ class Address(models.Model):
 
 
 class Email(models.Model):
+    id = models.AutoField(primary_key=True)
     i = models.BigIntegerField(blank=True, null=True)
     j = models.BigIntegerField(blank=True, null=True)
     server = models.TextField(blank=True, null=True)
@@ -57,3 +63,31 @@ class Email(models.Model):
         managed = True
         db_table = 'email'
         app_label = 'knowledgequest'
+
+
+def create_from_sqlite(path='/home/hobs/src/springboard/tannistha/enron_email.db', tables='Email Address'.split()):
+    with sqlite3.connect('/home/hobs/src/springboard/tannistha/enron_email.db') as con:
+        cur = con.cursor()
+        cur.execute('SELECT SQLITE_VERSION()')
+        data = cur.fetchone()
+        print("SQLite version: %s" % data)
+
+        for table_name in tables:
+            model = globals()[table_name]
+            print(model)
+            cur.execute('PRAGMA table_info({})'.format(table_name.lower()))
+            rows = cur.fetchall()
+            fields = [row[1] for row in rows]
+            print('sqfields: ', fields)
+            cur2 = con.cursor()
+            cur2.execute('SELECT * FROM {}'.format(table_name.lower()))
+            rows = cur2.fetchall()
+            for i, row in tqdm(enumerate(rows)):
+                djfields = [(s.lower() + '_field' if s in keywords else s.lower()) for s in fields]
+                if not i:
+                    print('djfields: ', djfields)
+                record = zip(djfields, row)
+                record = dict([(djfield, value) for (djfield, value, sqfield) in zip(djfields, row, fields) if hasattr(model, djfield)])
+                results = model.objects.get_or_create(**record)
+    
+
