c808b7949404f4e80a7040169c4b329e37f1d4d3
Hobson Lane <hobs@totalgood.com>
2018-12-30 07:06:30 -0800

    STATIC_ROOT moved to /var/www/knowledgequest/collected_static

--- src/knowledgequest/settings.py
+++ src/knowledgequest/settings.py
@@ -119,4 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+STATIC_ROOT = '/var/www/knowledgequest/collected_static/'
+
