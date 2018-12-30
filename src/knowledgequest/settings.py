7567117256fc82cf65d334b7ad210fa6d6cc3152
Hobson Lane <hobs@totalgood.com>
2018-12-30 12:11:58 -0800

    fix syntax error in settings for dATABASES

--- src/knowledgequest/settings.py
+++ src/knowledgequest/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, '..', '..', '..', 'src', 'springboard', 'tannistha', 'enron_email.db'),
-    }
+    },
     'postgres': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'knowledgequest',
