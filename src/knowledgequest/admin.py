8f8cb1e8b865082bba91f5c8a6f425db81a2a375
Hobson Lane <hobs@totalgood.com>
2018-12-31 16:30:01 -0800

    admin.py automagic for all models

--- src/knowledgequest/admin.py
+++ src/knowledgequest/admin.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
-from knowledgequest.models import Address, Email
+import knowledgequest.models
 
-for m in [Address, Email]:
-    print(m)
-    print(m.__class__)
+MODEL_CLASSES = [name for name, obj in vars(knowledgequest.models).items() if not name.startswith('_') and isinstance(obj, django.db.models.base.ModelBase)]
 
-class AddressAdmin(admin.ModelAdmin):
-    pass
 
-class EmailAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Address, AddressAdmin)
-admin.site.register(Email, EmailAdmin)
+for ModelClass in MODEL_CLASSES:
+    modeladmin_class_name = '{}Admin'.format(ModelClass.__name__)
+    class CustomModelAdmin(admin.ModelAdmin):
+        list_display = tuple([f.name for f in ModelClass._meta.get_fields()])
+    locals()[modeladmin_class_name] = CustomModelAdmin
+    ModelAdminClass = locals()[modeladmin_class_name]
+    try:
+        admin.site.register(ModelClass, ModelAdminClass)
+    except admin.sites.AlreadyRegistered:
+        print('{} is already registered'.format(ModelClass.__name__))
