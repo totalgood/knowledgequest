8e19f52d480e9fb5cca6dfe24dc08d2782046002
hobs <hobs+github@totalgood.com>
2019-01-23 13:53:14 -0800

    fixing-yaml

--- src/knowledgequest/focus.py
+++ src/knowledgequest/focus.py
@@ -5,16 +5,56 @@ import html2text
 import spacy
 import gensim
 from gensim.summarization.summarizer import summarize
+import textract
 
-html = open('/Users/hobs/Downloads/The PwC Internal Audit.html').read()
-txt = html2text.html2text(html)
 
-nlp = spacy.load('en')
-docobj = nlp(txt)
-sents = list(docobj.sents)
-str([w for w in sents[0]])
-tokenized_sents = [[str(w) for w in s] for s in sents]
+if __name__ == "__main__":
+    summaries = []
+    paths = [
+        '/Users/hobs/Downloads/The PwC Internal Audit.html',
+        '/Users/hobs/Downloads/Internal Audit - Risk Assurance - Assurance and Audit Services - PwC in the Middle East.html',
+        '/Users/hobs/Downloads/PWC-Expect More copy.pdf',
+    ]
+#     ## 5% summaries:
+#     [
+#      "The PwC Internal Audit  Close  ###### Start adding items to your reading lists:  Sign in  or  Create your account  \n
+#      *Regulatory good citizenship   ** Leading organisations that are regarded as 'regulatory good citizens' are those that
+#        embrace regulation, recognising that it drives resilience, good practice, control, competition, governance and performance.\n
+#      *Regulatory good citizenship   ** Leading organisations that are regarded as 'regulatory good citizens' are those that embrace regulation, 
+#        recognising that it drives resilience, good practice, control, competition, governance and performance.",
+#
+#      'The right skills and experience to help you identify, prioritise and gain assurance over the current and emerging risks that matter most to your business.',
+#
+#      'Stakeholders expect IA to ‘look deeper and see  further’, acting as a lever for change supporting an  organisation’s strategic agenda moving up from  an 
+#       assurance provider to trusted advisor (fig.\nTrusted  advisor  Providing value-added services  and proactive strategic advice  to the business well beyond 
+#       the  effective and efficient execution  of the audit plan.\nIn this time of constantly changing business  terrain, business alignment, risk focus, talent
+#       model and technology are four key areas  recognised by Chief Audit Executives (CAE) and  key stakeholders as top enablers for IA to be a  valued contributor.'
+#     ]
+#     ## 8% summaries:
+#     ["The PwC Internal Audit  Close  ###### Start adding items to your reading lists:  Sign in  or  Create your account  \n## **Regulatory Response – Redefining the value of regulation\n** It may seem like a paradox, but if you sort it out and do it well, a good regulatory response buys you a lot of freedom, allowing you to invest in your business with less scrutiny.\n*Regulatory good citizenship   ** Leading organisations that are regarded as 'regulatory good citizens' are those that embrace regulation, recognising that it drives resilience, good practice, control, competition, governance and performance.\n*Regulatory good citizenship   ** Leading organisations that are regarded as 'regulatory good citizens' are those that embrace regulation, recognising that it drives resilience, good practice, control, competition, governance and performance.",
+#      'These attributes are:    * Having an internal audit function focusing on the risks that matter to your organisation, both current and emerging.\nThe right skills and experience to help you identify, prioritise and gain assurance over the current and emerging risks that matter most to your business.',
+#      'Stakeholders expect IA to ‘look deeper and see  further’, acting as a lever for change supporting an  organisation’s strategic agenda moving up from  an assurance provider to trusted advisor (fig.\nTrusted  advisor  Providing value-added services  and proactive strategic advice  to the business well beyond the  effective and efficient execution  of the audit plan.\nIn this time of constantly changing business  terrain, business alignment, risk focus, talent  model and technology are four key areas  recognised by Chief Audit Executives (CAE) and  key stakeholders as top enablers for IA to be a  valued contributor.\nOutsourcing to manage your cost more effectively   •  Developing risk assessments, Internal Audit plans and scope \nDelivering value to your business  To help companies meet the demands of  maintaining an effective internal audit function,  we offer flexible, scalable, sophisticated solutions to  address your unique needs.']
 
-summarize([str(s) for s in sents].join('\n'))
-summarize('\n'.join([str(s).replace(' ', '\n') for s in sents]))
-sents
+
+
+    for path in paths:
+        ext = path.lower().split('.')[-1]
+        if ext == 'html':
+            html = open(path).read()
+            txt = html2text.html2text(html)
+        elif ext == 'pdf':
+            txt = textract.process(path).decode()
+
+        nlp = spacy.load('en')
+        docobj = nlp(txt)
+        sents = [str(s.text) for s in docobj.sents if '.html' not in s.text and 'http' not in s.text and len(s) > 2]
+        tokenized_sents = [[str(w) for w in s] for s in sents]
+
+        # summarize([str(s) for s in sents].join('\n'))
+        txt = '\n'.join([str(s).replace('\n', ' ') for s in sents])
+        summary = summarize(txt, .08)
+        print(txt)
+        print()
+        print()
+        print(summary)
+        summaries.append(summary)
--- src/knowledgequest/stack.py
+++ src/knowledgequest/stack.py
@@ -3,38 +3,108 @@ import time
 
 import pandas as pd
 from stackauth import StackAuth
-from stackexchange import Site, StackOverflow
+import stackexchange as se
+# from stackexchange import Site, StackOverflow
+
+DEFAULT_USER_ID = 623735  # hobs
+DEFAULT_SITE = 'stackoverflow'
+IGNORE_FIELDS = set(['json', 'json_ob', 'site'])
 
 
-def get_accounts(user_id=623735, total=20, sleep=3.14, **kwargs):
+def get_site(site=DEFAULT_SITE, user_id=DEFAULT_USER_ID):
+    return se.Site(site.lower().strip()) if isinstance(site, str) else site
+
+
+def get_accounts(total=20, site='stackoverflow', user_id=None):
+    site = get_site(site)
+    user_id = user_id or DEFAULT_USER_ID
     stack_auth = StackAuth()
-    so = Site(StackOverflow)
-    accounts = stack_auth.associated(so, user_id)
+    accounts = stack_auth.associated(site, user_id)
     reputation = {}
     
     for account in accounts:
-        print('  %s: %s / %d reputation' % ('hobs', account.on_site.name, account.reputation))
+        # print('  %s: %s / %d reputation' % ('hobs', account.on_site.name, account.reputation))
 
         reputation[account.reputation] = account.on_site.name
 
     print('Most reputation on: %s' % reputation[max(reputation)])
-    return 
+    return pd.DataFrame(sorted(reputation.items(), reverse=True), columns='reputation site'.split())
 
 
-def get_questions(total=20, sleep=3.14, **kwargs):
-    so = StackOverflow()
-    defaults = dict(pagesize=10) 
+def good_vars(obj):
+    return dict([(k, v) for k, v in vars(obj).items() if not k.startswith('_') and not k in IGNORE_FIELDS])
+
+
+def good_dict(d):
+    good = {}
+    if hasattr(d, 'keys') and callable(d.keys):
+        for k in d.keys():
+            if not isinstance(k, (str, int, float)):
+                continue
+            v = d[k]
+            if isinstance(v, (str, int, float)) or v is None:
+                good[k] = v 
+            elif isinstance(v, (tuple, list)):
+                good[k] = tuple([good_dict(obj) for obj in v])
+            elif isinstance(v, dict):
+                good[k] = good_dict(v)
+            elif hasattr(v, 'fetch'):
+                good[k] = tuple([good_dict(obj) for obj in v])
+    else:
+        good = d
+    return good
+
+
+def fetch_related():
+    """ Need a function to automatically fetch or empty "<unfetched sequence: Comment>" "<unfetched sequence: PostRevision>" etc """
+    pass
+
+
+def get_site(site=DEFAULT_SITE, user_id=DEFAULT_USER_ID):
+    return se.Site(site.lower().strip()) if isinstance(site, str) else site
+
+
+def get_questions(total=10, site='stackoverflow', sleep=3.14, **kwargs):
+    stack_auth = StackAuth()
+    site = se.Site(site.lower().strip()) if isinstance(site, str) else site
+    defaults = dict(pagesize=10, body=True, comments=True) 
     defaults.update(kwargs)
     questions = []
-    while len(questions) < total:
-        for q in so.questions(**defaults):
-            print(vars(q))
-            print(q.title)
-            questions.append(q.__dict__)
-        time.sleep(sleep)
-    return pd.DataFrame(questions)
+    answers = []
+    for q in site.questions(**defaults):
+        print(q.title)
+        qdict = good_vars(q)
+        for k in qdict.keys():
+            obj = qdict[k]
+            if hasattr(obj, 'fetch') and callable(obj.fetch):
+                obj = tuple(list(obj.fetch() or []))
+                if obj:
+                    qdict[k] = tuple([good_vars(v) if hasattr(v, '__dict__') else v for v in obj])
+            elif isinstance(obj, list):
+                qdict[k] = tuple([good_vars(v) if hasattr(v, '__dict__') else v for v in obj])
+        qans = list(qdict.pop('answers', []))
+        qdict['answers'] = [ans['id'] for ans in qans]
+        qdict['accepted_answer'] = [ans['id'] for ans in qans if ans['is_accepted']]
+        qdict['accepted_answer'] = qdict['accepted_answer'][0] if qdict['accepted_answer'] else None
+        answers.extend(qans)
+        questions.append(qdict)
+        if len(questions) >= total:
+            break
+        if not len(questions) % defaults['pagesize']:
+            print('Sleeping for {} seconds'.format(sleep))
+            time.sleep(sleep)
+    return questions, answers
 
 
 if __name__ == '__main__':
-    user_id = 623735 if len(sys.argv) < 2 else int(sys.argv[1])
+    if len(sys.argv) > 1:
+        total = int(sys.argv[1])
+        accounts = get_accounts()
+        questions, answers = get_questions(total=total)
+        answers = pd.DataFrame(answers)
+        answers.to_csv('answers.csv')
+        questions = pd.DataFrame(questions)
+        questions.to_csv('questions.csv')
+
+    # print(get_questions())
     # print('Using stackOverflow user %d\'s accounts:' % user_id)
