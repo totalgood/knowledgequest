8c092e81f9d101450d8a1b16208732475883cbbd
hobs <hobs+github@totalgood.com>
2019-05-19 20:29:16 -0700

    move faq_psychology add more models.py

--- src/knowledgequest/data/models.py
+++ src/knowledgequest/data/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
+
 class User(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255)
+    real_name = models.CharField(
+        null=True, blank=True, max_length=255,
+        help_text="""You can optionally provide your full name in order to help friends find you.
+                     For each connection request you will be allowed to respond without revealing your real name, unless you choose to.
+                  """)
+    public_name = models.CharField(null=False, blank=True, max_length=32,
+                                   help_text="This is the public username that others will see associated with your contributions.")
+    reddit_username = models.CharField(null=True, blank=True, max_length=32)
+    # email = models.EmailField(max_length=255)
+
 
 class Email(models.Model):
-    pass
-
+    user = models.ForeignKey(
+        User, null=False, blank=True,
+        help_text="""You must have at least one verified email address associated with your account.
+                     We never share any of your contact information with anyone.""")
